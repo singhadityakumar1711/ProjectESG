9959caf21fb62a822ce4b901e1db904293c8615e
Aditya Kumar Singh <singhadityakumar1711@gmail.com>
2024-06-28 21:29:59 +0530

    Summarize endpoint created

---
+++ django_mvp-1/serve/summarize.py
@@ -0,0 +1,26 @@
+import os
+from llama_index.core import VectorStoreIndex
+from llama_index.core import SimpleDirectoryReader
+
+# Set the OpenAI API key
+os.environ["OPENAI_API_KEY"] = "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"
+
+
+def generate_response():
+    documents = SimpleDirectoryReader(
+        input_files=["C:/Users/ASUS/Desktop/GreenR/tcs.pdf"]
+    ).load_data()
+    # print(documents[:5])
+    # Create the index
+    index = VectorStoreIndex.from_documents(documents)
+
+    # Create a query engine
+    query_engine = index.as_query_engine()
+
+    # Query the engine
+    response = query_engine.query(
+        "What is TCS doing to curtail it's GHG Emissions.Please elaborate in bullet points"
+    )
+
+    # Print the response
+    return f"Response to: {response.response}"
