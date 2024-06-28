1ff989f67c87e3bc3bf03e7368679b7854d753a7
Aditya Kumar Singh <singhadityakumar1711@gmail.com>
2024-06-28 21:52:17 +0530

    Summarize endpoint 2

--- django_mvp-1/serve/summarize.py
+++ django_mvp-1/serve/summarize.py
@@ -1,9 +1,12 @@
 import os
 from llama_index.core import VectorStoreIndex
 from llama_index.core import SimpleDirectoryReader
+from dotenv import load_dotenv
 
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 # Set the OpenAI API key
-os.environ["OPENAI_API_KEY"] = "OPENAI_KEY"
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
 
 def generate_response():
